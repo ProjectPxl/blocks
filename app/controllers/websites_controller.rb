ce7e3d9f3add8b8073889377a15b5172b6f00dd6
Paul Maltsev <maltsevp@gmail.com>
2017-06-01 14:13:02 -0700

    adding susy 2

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -1,2 +1,13 @@
 class WebsitesController < ApplicationController
+	layout 'website', only: [:index]
+
+	def index
+		@website = Website.first
+		@title 	 = @website.name
+		@source  = @website.components.first.html.html_safe
+	end
+
+	def show
+		@name = Website.find(params[:id]).name
+	end
 end
